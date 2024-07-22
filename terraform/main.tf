6429d70074c2e4f6794e6256e20fd779fb898e3c
Max Zapletin <maxim@captain-eye.com>
2024-07-22 20:51:32 +0300

    Revert " adding configuration files"
    This reverts commit c8feb7a5b71fe51def2e6fddb5c9d7cba84e62bd.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  profile = "max"
-  region  = "eu-central-1"  
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "max-private-bucket-test2" 
+  
 }
