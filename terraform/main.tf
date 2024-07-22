c8feb7a5b71fe51def2e6fddb5c9d7cba84e62bd
Max Zapletin <maxim@captain-eye.com>
2024-07-22 20:49:43 +0300

    adding configuration files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  
+  profile = "max"
+  region  = "eu-central-1"  
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "max-private-bucket-test2" 
 }
