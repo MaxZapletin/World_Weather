453e99bc8c0b9fe8aa466e4d4dff566bd106aa84
Max Zapletin <maxim@captain-eye.com>
2024-07-26 22:13:19 +0300

    build

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,19 +1,18 @@
-# Create IAM Policy
 resource "aws_iam_policy" "github_actions_policy" {
   name        = "github-actions-ecr-ecs-eks-policy"
   path        = "/"
   description = "IAM policy for GitHub Actions with ECR, ECS, EC2, and EKS permissions"
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = ["ecr:*"]
+        Effect   = "Allow",
+        Action   = ["ecr:*"],
         Resource = "*"
       },
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "ecs:ListClusters",
           "ecs:DescribeClusters",
@@ -31,11 +30,11 @@ resource "aws_iam_policy" "github_actions_policy" {
           "ecs:StopTask",
           "ecs:ListTasks",
           "ecs:DescribeTasks"
-        ]
+        ],
         Resource = "*"
       },
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "ec2:DescribeInstances",
           "ec2:DescribeInstanceStatus",
@@ -45,11 +44,11 @@ resource "aws_iam_policy" "github_actions_policy" {
           "ec2:DescribeSecurityGroups",
           "ec2:DescribeKeyPairs",
           "ec2:DescribeImages"
-        ]
+        ],
         Resource = "*"
       },
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "eks:ListClusters",
           "eks:DescribeCluster",
@@ -58,24 +57,37 @@ resource "aws_iam_policy" "github_actions_policy" {
           "eks:ListUpdates",
           "eks:DescribeUpdate",
           "eks:AccessKubernetesApi"
-        ]
+        ],
         Resource = "*"
       }
     ]
   })
 }
 
-resource "aws_iam_user" "github_actions_user" {
-  name = "github-actions-user"
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-role"
   path = "/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "sts.amazonaws.com"
+        },
+        Action = "sts:AssumeRole",
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId": "Weather-AP/World_Weather"
+          }
+        }
+      }
+    ]
+  })
 }
 
-resource "aws_iam_user_policy_attachment" "github_actions_user_policy" {
-  user       = aws_iam_user.github_actions_user.name
+resource "aws_iam_role_policy_attachment" "github_actions_role_policy" {
+  role       = aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.github_actions_policy.arn
-}
-
-output "github_actions_user_name" {
-  value       = aws_iam_user.github_actions_user.name
-  description = "name of the IAM user"
 }
