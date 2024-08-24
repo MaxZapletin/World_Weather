947cd97c85c2d99ca70c7494e265f2833fbdae8d
Max Zapletin <maxim@captain-eye.com>
2024-08-24 13:35:44 +0300

    adding execution role for ecs

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -65,3 +65,23 @@ resource "aws_iam_user_policy_attachment" "github_actions_user_policy" {
   user       = aws_iam_user.github_actions_user.name
   policy_arn = aws_iam_policy.github_actions_policy.arn
 }
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
