260566cef3d9043f3c7dbd21012b3f7507663ad3
Max Zapletin <maxim@captain-eye.com>
2024-08-24 11:24:11 +0300

    addint ECR var for ECS

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -4,6 +4,7 @@ resource "aws_ecs_cluster" "main" {
   setting {
     name  = "containerInsights"
     value = "enabled"
+    repository_url           = module.ecr.weather_repository_url
   }
 
   tags = {
@@ -59,23 +60,3 @@ resource "aws_ecs_task_definition" "app_template" {
   }
 }
 
-# Outputs
-output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.main.name
-  description = "Name of the ECS cluster"
-}
-
-output "ecs_cluster_arn" {
-  value       = aws_ecs_cluster.main.arn
-  description = "ARN of the ECS cluster"
-}
-
-output "ecs_execution_role_arn" {
-  value       = aws_iam_role.ecs_execution_role.arn
-  description = "ARN of the ECS task execution role"
-}
-
-output "task_definition_arn" {
-  value       = aws_ecs_task_definition.app_template.arn
-  description = "ARN of the task definition template"
-}
---
+++ terraform/ecs/variables.tf
@@ -0,0 +1,4 @@
+variable "ecr_repository_url" {
+  description = "The URL ECR repository"
+  type        = string
+}
